0f539864730c51f207f1b5a2253b9764bfdd330c
pratio <6872952+pratio@users.noreply.github.com>
2024-05-26 18:14:27 +0200

    Fix roles

--- systems/sales/main.tf
+++ systems/sales/main.tf
@@ -1,31 +1,30 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
+      source = "Snowflake-Labs/snowflake"
       version = "~> 0.76"
     }
   }
 }
 
-
 module "sales_database" {
-  source = "../../modules/database"
-  name   = "EXT_SALESFORCE_DB"
+  source  = "../../modules/database"
+  name    = var.database_name
   schemas = ["schema1", "schema2"]
 }
 
 module "sales_warehouse" {
-  source      = "../../modules/warehouse"
-  name        = "EXT_SALESFORCE_WH"
-  size        = "X-Small"
+  source       = "../../modules/warehouse"
+  name         = var.warehouse_name
+  size         = "X-Small"
   auto_suspend = 60
 }
 
 module "sales_role" {
   source             = "../../modules/role"
-  name               = "EXT_SALESFORCE_USAGE"
+  name               = var.name
   database_name      = module.sales_database.database_name
-  database_privilege = "USAGE"
+  database_privilege = var.database_privilege
   warehouse_name     = module.sales_warehouse.warehouse_name
-  warehouse_privilege = "USAGE"
+  warehouse_privilege = var.warehouse_privilege
 }
--- systems/sales/variables.tf
+++ systems/sales/variables.tf
@@ -1,16 +1,19 @@
 variable "name" {
   type        = string
   description = "Name of the role"
+  default     = "EXT_SALESFORCE_USAGE"
 }
 
 variable "database_name" {
   type        = string
   description = "Name of the database to grant access"
+  default     = "EXT_SALESFORCE_DB"
 }
 
 variable "warehouse_name" {
   type        = string
   description = "Name of the warehouse to grant access"
+  default     = "EXT_SALESFORCE_WH"
 }
 
 variable "database_privilege" {
@@ -23,4 +26,10 @@ variable "warehouse_privilege" {
   type        = string
   description = "Privilege for the warehouse"
   default     = "USAGE"
+}
+
+variable "snowflake_role" {
+  type        = string
+  description = "Role used for deployment"
+  sensitive   = true
 }
