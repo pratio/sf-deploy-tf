94f395adfb01a0f69e5c1191568accd6e89d59cb
pratio <6872952+pratio@users.noreply.github.com>
2024-05-26 18:24:07 +0200

    Fix roles

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,27 +1,35 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.76"
-    }
-  }
-}
-
-resource "snowflake_database" "database" {
+resource "snowflake_role" "role" {
   name = var.name
 
   lifecycle {
-    prevent_destroy = true
-    ignore_changes  = all
-  }
-
-  provisioner "local-exec" {
-    command = "echo Database ${self.name} already exists."
-    when    = create
-    on_failure = continue
+    ignore_changes = all
   }
 }
 
-output "database_name" {
-  value = snowflake_database.database.name
+resource "snowflake_role_grants" "database_grant" {
+  role_name  = snowflake_role.role.name
+  object_type = "database"
+  object_name = var.database_name
+
+  privileges = [var.database_privilege]
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "snowflake_role_grants" "warehouse_grant" {
+  role_name  = snowflake_role.role.name
+  object_type = "warehouse"
+  object_name = var.warehouse_name
+
+  privileges = [var.warehouse_privilege]
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+output "role_name" {
+  value = snowflake_role.role.name
 }
--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -1,9 +1,24 @@
 variable "name" {
   type        = string
-  description = "Name of the database"
+  description = "Name of the role"
 }
 
-variable "schemas" {
-  type        = list(string)
-  description = "List of schemas"
+variable "database_name" {
+  type        = string
+  description = "Name of the database to grant access"
+}
+
+variable "database_privilege" {
+  type        = string
+  description = "Privilege for the database"
+}
+
+variable "warehouse_name" {
+  type        = string
+  description = "Name of the warehouse to grant access"
+}
+
+variable "warehouse_privilege" {
+  type        = string
+  description = "Privilege for the warehouse"
 }
